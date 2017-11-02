367e0f295abff1c603f881aaaf5d8e7cfe1bddc9
Maarten de Vries <maarten@de-vri.es>
2017-11-02 23:20:36 +0100

    Add initial traits.

---
+++ include/estd/traits/containers.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <type_traits>
+#include <utility>
+
+namespace estd {
+
+template<typename T> using iterator_type       = decltype(std::declval<T>().begin());
+template<typename T> using const_iterator_type = decltype(std::declval<T const>().begin());
+
+namespace detail {
+	using std::declval;
+
+	template<typename T, typename = void> struct has_begin : std::false_type {};
+	template<typename T> struct has_begin<T, std::void_t<decltype(declval<T>().begin())>> : std::true_type {};
+
+	template<typename T, typename = void> struct has_end : std::false_type {};
+	template<typename T> struct has_end<T, std::void_t<decltype(declval<T>().end())>> : std::true_type {};
+
+	template<typename T, typename = void> struct has_size : std::false_type {};
+	template<typename T> struct has_size<T, std::void_t<decltype(declval<T>().size())>> : std::true_type {};
+
+	template<typename T, typename = void> struct has_reserve : std::false_type {};
+	template<typename T> struct has_reserve<T, std::void_t<decltype(declval<T>().reserve(30))>> : std::true_type {};
+
+	template<typename T, typename = void> struct has_erase : std::false_type {};
+	template<typename T> struct has_erase<T, std::void_t<decltype(declval<T>().erase(declval<const_iterator_type<T>>(), declval<const_iterator_type<T>>()))>> : std::true_type {};
+}
+
+/// Test if a type T is iterable (has a begin() and end() member)
+template<typename T> constexpr bool is_iterable = detail::has_begin<T>() && detail::has_end<T>();
+
+/// Test if a type T has a size() member.
+template<typename T> constexpr bool has_size = detail::has_size<T>();
+
+/// Test if a type T has a reserve() member.
+template<typename T> constexpr bool has_reserve = detail::has_reserve<T>();
+
+/// Test if a type T has a erase() member.
+template<typename T> constexpr bool has_erase = detail::has_erase<T>();
+
+/// Test if a type T is a container.
+/**
+ * Currently this is a relaxed check. It only tests if the members begin(), end() and size() exist.
+ */
+template<typename T> constexpr bool is_container = is_iterable<T> && has_size<T>;
+
+}
---
+++ test/traits/container.cpp
@@ -0,0 +1,39 @@
+#include "traits/containers.hpp"
+
+#include <vector>
+#include <string>
+#include <map>
+
+namespace estd {
+
+static_assert(is_iterable  <int> == false);
+static_assert(has_size     <int> == false);
+static_assert(has_reserve  <int> == false);
+static_assert(has_erase    <int> == false);
+static_assert(is_container <int> == false);
+
+static_assert(is_iterable  <std::vector<int>> == true);
+static_assert(has_size     <std::vector<int>> == true);
+static_assert(has_reserve  <std::vector<int>> == true);
+static_assert(has_erase    <std::vector<int>> == true);
+static_assert(is_container <std::vector<int>> == true);
+static_assert(std::is_same_v<      iterator_type<std::vector<int>>, std::vector<int>::iterator>);
+static_assert(std::is_same_v<const_iterator_type<std::vector<int>>, std::vector<int>::const_iterator>);
+
+static_assert(is_iterable  <std::string> == true);
+static_assert(has_size     <std::string> == true);
+static_assert(has_reserve  <std::string> == true);
+static_assert(has_erase    <std::string> == true);
+static_assert(is_container <std::string> == true);
+static_assert(std::is_same_v<      iterator_type<std::string>, std::string::iterator>);
+static_assert(std::is_same_v<const_iterator_type<std::string>, std::string::const_iterator>);
+
+static_assert(is_iterable  <std::map<int, int>> == true);
+static_assert(has_size     <std::map<int, int>> == true);
+static_assert(has_reserve  <std::map<int, int>> == false);
+static_assert(has_erase    <std::map<int, int>> == true);
+static_assert(is_container <std::map<int, int>> == true);
+static_assert(std::is_same_v<      iterator_type<std::map<int, int>>, std::map<int, int>::iterator>);
+static_assert(std::is_same_v<const_iterator_type<std::map<int, int>>, std::map<int, int>::const_iterator>);
+
+}
