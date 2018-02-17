5e594844161274a867180e2b3916ae261c47c9a3
Maarten de Vries <maarten@de-vri.es>
2018-02-17 21:20:44 +0100

    tuple: add for_each_i which also gives the index to the functor.

--- include/estd/tuple/for_each.hpp
+++ include/estd/tuple/for_each.hpp
@@ -23,10 +23,28 @@ namespace detail {
 			return tuple_for_each_impl<I + 1>(std::forward<Tuple>(tuple), std::forward<F>(f));
 		}
 	}
+
+	template<std::size_t I, typename F, typename Tuple>
+	constexpr std::size_t tuple_for_each_i_impl(Tuple && tuple, F && f) {
+		if constexpr (I == std::tuple_size_v<std::decay_t<Tuple>>) {
+			return I;
+		} else {
+			using std::get;
+			using R = decltype(std::invoke(f, I, get<I>(std::forward<Tuple>(tuple))));
+			if constexpr(std::is_same_v<R, void>) {
+				std::invoke(f, I, get<I>(std::forward<Tuple>(tuple)));
+			} else {
+				if (!std::invoke(f, I, get<I>(std::forward<Tuple>(tuple)))) return I;
+			}
+			return tuple_for_each_i_impl<I + 1>(std::forward<Tuple>(tuple), std::forward<F>(f));
+		}
+	}
 }
 
 /// Execute a functor for each tuple element in order.
 /**
+ * The functor willed be called as func(elem) where elem is the value of the element.
+ *
  * The functor may optionally return a value to abort the loop before all elements are processed.
  * If the functor returns a value it is tested in boolean context.
  * If the value compares as false, the loop is stopped.
@@ -35,8 +53,25 @@ namespace detail {
  * or the size of the tuple if no element caused the loop to stop.
  */
 template<typename F, typename Tuple>
-constexpr std::size_t for_each(Tuple && tuple, F && f) {
-	return detail::tuple_for_each_impl<0>(std::forward<Tuple>(tuple), std::forward<F>(f));
+constexpr std::size_t for_each(Tuple && tuple, F && func) {
+	return detail::tuple_for_each_impl<0>(std::forward<Tuple>(tuple), std::forward<F>(func));
+}
+
+/// Execute a functor for each tuple element in order.
+/**
+ * The functor willed be called as func(i, elem) where i is the index in the tuple,
+ * and elem is the value of the element.
+ *
+ * The functor may optionally return a value to abort the loop before all elements are processed.
+ * If the functor returns a value it is tested in boolean context.
+ * If the value compares as false, the loop is stopped.
+ *
+ * Returns the index of the last element that cause the loop to halt,
+ * or the size of the tuple if no element caused the loop to stop.
+ */
+template<typename F, typename Tuple>
+constexpr std::size_t for_each_i(Tuple && tuple, F && func) {
+	return detail::tuple_for_each_i_impl<0>(std::forward<Tuple>(tuple), std::forward<F>(func));
 }
 
 }
--- test/tuple/for_each.cpp
+++ test/tuple/for_each.cpp
@@ -37,4 +37,51 @@ TEST_CASE("tuple for_each", "[tuple]") {
 	}
 }
 
+TEST_CASE("tuple for_each_i", "[tuple]") {
+	SECTION("processes each element with the right index") {
+		int sum = 0;
+		auto tuple = std::make_tuple(0, 1, 2, 3);
+		std::size_t index = for_each_i(tuple, [&] (int i, auto v) {
+			REQUIRE(i == v);
+			sum += v;
+		});
+		REQUIRE(index == 4);
+		REQUIRE(sum   == 6);
+	}
+
+	SECTION("processes each element if functor returns true") {
+		int sum = 0;
+		auto tuple = std::make_tuple(0, 1, 2, 3);
+		std::size_t index = for_each_i(tuple, [&] (int i, auto v) {
+			REQUIRE(i == v);
+			sum += v;
+			return true;
+		});
+		REQUIRE(index == 4);
+		REQUIRE(sum   == 6);
+	}
+
+	SECTION("stops when functor returns false") {
+		int sum = 0;
+		auto tuple = std::make_tuple(0, 1, 2, 3);
+		std::size_t index = for_each_i(tuple, [&] (int i, auto v) {
+			REQUIRE(i == v);
+			sum += v;
+			return i != 2;
+		});
+		REQUIRE(index == 2);
+		REQUIRE(sum   == 3);
+	}
+
+	SECTION("reports index if stopped") {
+		auto tuple = std::make_tuple(1, 2, 3, 4);
+		REQUIRE(for_each_i(tuple, [&] (std::size_t i, auto) { return i != 3; }) == 3);
+	}
+
+	SECTION("reports tuple size if not stopped") {
+		auto tuple = std::make_tuple(1, 2, 3, 4);
+		REQUIRE(for_each_i(tuple, [&] (std::size_t, auto) { return true; }) == 4);
+	}
+}
+
 }
