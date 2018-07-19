a11c0fda9d5e24d2546bef58a641db14e384cb00
Maarten de Vries <maarten@de-vri.es>
2018-07-19 16:25:08 +0200

    convert: let parse() fall back to regular conversion.

--- include/estd/convert/convert.hpp
+++ include/estd/convert/convert.hpp
@@ -27,47 +27,16 @@
  */
 
 #pragma once
-#include "../result.hpp"
+#include "./conversion.hpp"
+
+#include "./traits.hpp"
+#include "../result/result.hpp"
+#include "../result/error.hpp"
 
 #include <type_traits>
 #include <utility>
 
 namespace estd {
-
-struct default_conversion {};
-
-/// Definition of a conversion.
-/**
- * You may define your own conversions by specializing this struct.
- *
- * To define special conversions for your own use, you may specialize on a different Tag type.
- * These special conversions can be invoked by passing the same tag to `estd::convert`.
- *
- * It is also possible to mark a conversion as impossible explicitly.
- * A conversion is considered impossible when any of these conditions is true:
- *   - The specialization has a member `static constexpr bool possible = false`.
- *   - The specialization has no member named `perform`.
- *
- * The actual conversion function must be implemented as:
- *   static To perform(From ... from)
- *
- * The `from` argument may be passed by value, reference or rvalue reference.
- * It is also allowed to define multiple overloads, or to implement the `perform` member
- * as a function template.
- */
-template<typename From, typename To, typename Tag = default_conversion>
-struct conversion {
-	static constexpr bool possible = std::is_constructible_v<To, From>;
-
-	static To perform(From const & from) {
-		static_assert(std::is_constructible_v<To, From>, "cannot convert From to To");
-		return To(from);
-	}
-	static To perform(From && from) {
-		static_assert(std::is_constructible_v<To, From>, "cannot convert From to To");
-		return To(from);
-	}
-};
 
 /// Specializable struct to define the default error type for parsing a T from an F.
 template<typename F, typename T, typename Tag = default_conversion>
@@ -91,7 +60,11 @@ T convert(F && from) {
  */
 template<typename T, typename E, typename Tag = default_conversion, typename F>
 result<T, E> parse(F && from) {
-	return convert<result<T, E>, Tag>(std::forward<F>(from));
+	if constexpr (!estd::can_parse<F, T, E, Tag> && estd::can_convert<F, T, Tag>) {
+		return {estd::in_place_valid, convert<T, Tag>(std::forward<F>(from))};
+	} else {
+		return convert<result<T, E>, Tag>(std::forward<F>(from));
+	}
 }
 
 /// Convert a value to a result<T>.
@@ -100,7 +73,9 @@ result<T, E> parse(F && from) {
  */
 template<typename T, typename F>
 result<T, default_parse_error<F, T>> parse(F && from) {
-	return convert<result<T, default_parse_error<F, T>>, default_conversion>(std::forward<F>(from));
+	using E   = default_parse_error<F, T>;
+	using Tag = default_conversion;
+	return parse<T, E, Tag>(std::forward<F>(from));
 }
 
 }
